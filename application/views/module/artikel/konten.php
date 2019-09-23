9642a6cc2deac1fc27fde62410e5f04f9a5acf9d
opojare <kurodyami@gmail.com>
2019-09-23 17:06:39 +0700

    add konten

--- application/views/module/artikel/konten.php
+++ application/views/module/artikel/konten.php
@@ -2,7 +2,7 @@
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
         <li class="breadcrumb-item active"><a href="<?= base_url(''); ?>">Beranda</a></li>
-        <li class="breadcrumb-item active"><a href="<?= base_url('artikel/index'); ?>">Artikel</a></li>
+        <li class="breadcrumb-item active"><a href="<?= base_url('homepage/artikel'); ?>">Artikel</a></li>
         <li class="breadcrumb-item active" aria-current="page">Isi Artikel</li>
     </ol>
 </nav>
@@ -11,10 +11,22 @@
 <div class="containter">
     <div class="row">
         <div class="col-md-12">
-            <div class="card shadow">
 
+            <!-- Basic Card Example -->
+            <div class="card shadow mb-4">
+                <div class="container">
+
+                    <img src="<?= base_url('assets/img/1.jpg'); ?>" class="card-img-top mt-4" alt="...">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title text-uppercase text-center">Judul</h3>
+                    <p class="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil dolore nobis officia officiis. Nihil magni enim voluptatum, amet ipsum excepturi eius doloribus reiciendis dolor perspiciatis. Necessitatibus voluptatum temporibus illo explicabo!</p>
+                    <p class="card-text float-right"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
             </div>
+
         </div>
     </div>
+</div>
 </div>
 <!-- isi end -->
